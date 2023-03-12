0fb87ee8c578a0af904c66966845e00f57a0f1b5
vwout <github15@info.vwout.nl>
2023-03-12 23:32:24 +0100

    Fixes in message parsing

--- c3/core.py
+++ c3/core.py
@@ -1,7 +1,7 @@
 import consts
 import crc
 import socket
-import utils
+import logging
 
 
 class C3:
@@ -10,41 +10,44 @@ class C3:
         self._sock.settimeout(2)
         self.session_id: int = None
         self.request_nr: int = 0
+        self.log = logging.getLogger("C3")
+        self.log.setLevel(logging.ERROR)
 
     @classmethod
-    def _get_message_header(cls, data: [bytes or bytearray]) -> tuple([int or None], int):
+    def _get_message_header(cls, data: [bytes or bytearray]) -> tuple[[int or None], int]:
         command = None
         data_size = 0
 
         if len(data) >= 5:
-            if data[0] == consts.C3_MESSAGE_START and data[1] ==  consts.C3_PROTOCOL_VERSION:
-                command = data[3]
-                data_size = data[4] + (data[5] * 255)
+            if data[0] == consts.C3_MESSAGE_START: #and data[1] == consts.C3_PROTOCOL_VERSION:
+                command = data[2]
+                data_size = data[3] + (data[4] * 255)
 
-          return command, data_size
+        return command, data_size
 
-    @classmethod
-    def _get_message(cls, data: [bytes or bytearray]) -> [bytearray or None]:
-        message = None
-        if data[:-1] == consts.C3_MESSAGE_END:
+    def _get_message(self, data: [bytes or bytearray]) -> bytearray:
+        message = bytearray()
+        if data[-1] == consts.C3_MESSAGE_END:
             # Get the message payload, without start, crc and end bytes
-            message = bytearray(data[1:-3])
-            checksum = crc.crc16(message)
+            checksum = crc.crc16(data[1:-3])
 
-            if utils.lsb(checksum) == data[:-3] and utils.msb(checksum) == data[:-2]:
-                # Remove the header(4 bytes)
-                del message[0:3]
+            if utils.lsb(checksum) == data[-3] or utils.msb(checksum) == data[-2]:
+                message = bytearray(data[5:-3])
+            else:
+                self.log.debug("Payload checksum is invalid: %s expected %x", data[-3:-2].hex(), checksum)
+        else:
+            self.log.debug("Payload does not include message end marker (%s)", data[-1])
 
         return message
 
-    def _send(self, command: consts.CommandStruct, data = None) -> int:
+    def _send(self, command: consts.CommandStruct, data=None) -> int:
         message_length = 0x04 + len(data or [])
         message = bytearray([consts.C3_PROTOCOL_VERSION,
                              command.request or 0x00,
                              utils.lsb(message_length),
                              utils.msb(message_length),
-                             utils.lsb(self.session_id),
-                             utils.msb(self.session_id),
+                             utils.lsb(self.session_id or 0),
+                             utils.msb(self.session_id or 0),
                              utils.lsb(self.request_nr),
                              utils.msb(self.request_nr)])
 
@@ -64,45 +67,66 @@ class C3:
         message.insert(0, consts.C3_MESSAGE_START)
         message.append(consts.C3_MESSAGE_END)
 
+        self.log.debug("Sending: %s", message.hex(' ', 1))
+
         bytes_written = self._sock.send(message)
         self.request_nr = self.request_nr + 1
         return bytes_written
 
-    def _receive(self, expected_command: consts.CommandStruct) -> [bytearray or None]:
-        size = 0
-        message = None
+    def _receive(self, expected_command: consts.CommandStruct) -> tuple[bytearray, int]:
+        message = bytearray()
 
         # Get the first 5 bytes
         header = self._sock.recv(5)
+        self.log.debug("Receiving header: %s", header.hex(' ', 1))
+
         received_command, data_size = self._get_message_header(header)
+
         if received_command == expected_command.reply:
             # Get the message data and signature
             payload = self._sock.recv(data_size + 3)
-            message = self._get_message(payload)
+            self.log.debug("Receiving payload: %s", payload.hex(' ', 1))
+            message = self._get_message(header + payload)
 
             if len(message) != data_size:
                 raise ValueError("Length of received message (%d) does not match specified size (%d)" % (len(message), data_size))
+        else:
+            data_size = 0
 
-        return size, message
+        return message, data_size
 
-    def _send_receive(self, command: consts.CommandStruct, data = None):
+    def _send_receive(self, command: consts.CommandStruct, data=None) -> tuple[bytearray, int]:
         bytes_received = 0
-        receive_data = None
-        response = None
+        receive_data = bytearray()
 
         bytes_written = self._send(command, data)
         if bytes_written > 0:
-            bytes_received, receive_data = self._receive(command)
+            receive_data, bytes_received = self._receive(command)
+
+        return receive_data, bytes_received
 
         return response, bytes_received, receive_data
 
-    def connect(self, host: str, port: int = consts.C3_PORT_DEFAULT):
+    def log_level(self, level: int):
+        self.log.setLevel(level)
+
+    def connect(self, host: str, port: int = consts.C3_PORT_DEFAULT) -> bool:
+        connected = False
+
         self._sock.connect((host, port))
-        self._send_receive(consts.C3_COMMAND_CONNECT)
+        receive_data, bytes_received = self._send_receive(consts.C3_COMMAND_CONNECT)
+        if bytes_received > 2:
+            self.session_id = (receive_data[1] << 8) + receive_data[0]
+            self.log.debug("Connected with Session ID %x", self.session_id)
+            connected = True
+        else:
+            self.session_id = 0
+
+        return connected
 
     def disconnect(self):
         self._send_receive(consts.C3_COMMAND_DISCONNECT)
         self._sock.close()
 
-        self.session_id = None
-        self.request_nr=  0
+        self.session_id = 0
+        self.request_nr = 0
