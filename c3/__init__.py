a2651e3fc93703c05ba5ccbeaef51c0b2d1a6086
vwout <github15@info.vwout.nl>
2023-03-19 17:47:41 +0100

    Setup module for build

--- c3/__init__.py
+++ c3/__init__.py
@@ -1,6 +1,8 @@
-# ZKAccess C3 library
+"""ZKAccess C3 library"""
 from .core import C3
+from . import controldevice
+from . import rtlog
 
 VERSION = (0, 0, 1)
 
-__all__ = ['C3']
+__all__ = ['C3', 'controldevice', 'rtlog']
